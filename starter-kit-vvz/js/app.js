204e80837d22dc35b8a1706345e5bb56136f2d4a
jens <jens@lea.io>
2014-01-05 14:28:34 +0100

    Lehrende und ihre Kurse

--- starter-kit-vvz/js/app.js
+++ starter-kit-vvz/js/app.js
@@ -11,3 +11,30 @@ App.CoursesRoute = Ember.Route.extend({
     return this.store.findAll('course');
   }
 });
+
+App.CoursesIndexController = Ember.ArrayController.extend({
+  needs: [ 'courses' ],
+  courses: function() {
+    return this.get('controllers.courses.model');
+  }.property('controllers.courses.model')
+});
+
+
+App.CourseController = Ember.ObjectController.extend({
+  lecturerNames: function() {
+    var l = this.get('lecturers');
+    return l.getEach('name').join(', ');
+  }.property('lecturers.@each'),
+    
+  lecturerCourses: function() {
+    var related = this.get('lecturers.firstObject.courses');
+    return related;
+  }.property('lecturers.firstObject')
+});
+
+
+
+
+
+
+
